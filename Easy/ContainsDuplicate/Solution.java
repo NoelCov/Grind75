8043bb422be3ee2c02af8bb6c81bb5426b9d1249
Noel <no3lcodes@gmail.com>
2023-08-02 13:25:08 -0700

    Added solution to problem 'Contains Duplicate'

---
+++ Easy/ContainsDuplicate/Solution.java
@@ -0,0 +1,15 @@
+class Solution {
+    // Time Complexity O(n)
+    // Space Complexity O(n)
+    public boolean containsDuplicate(int[] nums) {
+        Set<Integer> set = new HashSet<>();
+
+        for (int num : nums) {
+            if (set.contains(num)) {
+                return true;
+            }
+            set.add(num);
+        }
+        return false;
+    }
+}
